b4c1e16ebb150abb1d4b4719b37d2009adc07f40
alib2004 <aliw.b2004@gmail.com>
2025-02-21 11:37:56 +0330

    fixed bug

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
